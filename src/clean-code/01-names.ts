c96d0e25675fd8ce35ec2293a83bf9b3c9876bcb
Daniel Gómez <danielivgho5@gmail.com>
2023-10-31 13:32:27 -0500

    nombres de variables corregidos en el archivo 01-names.ts

--- src/clean-code/01-names.ts
+++ src/clean-code/01-names.ts
@@ -1,52 +1,52 @@
 (() => {
 
-    // Ejemplo
-    // Archivos a evaluar - files to evaluate
-    const fs = [
-        { id: 1, f: false },
-        { id: 2, f: false },
-        { id: 3, f: true },
-        { id: 4, f: false },
-        { id: 5, f: false },
-        { id: 7, f: true },
-    ]; 
-    
-    // Archivos marcados para borrar - files to delete
-    const arhivos = fs.map( f => f.f );
+  // Ejemplo
+  // Archivos a evaluar - files to evaluate
+  const filesToEvaluate = [
+    { id: 1, flagged: false },
+    { id: 2, flagged: false },
+    { id: 3, flagged: true },
+    { id: 4, flagged: false },
+    { id: 5, flagged: false },
+    { id: 7, flagged: true },
+  ];
 
-    
-    class AbstractUser { };
-    class UserMixin { };
-    class UserImplementation { };
-    interface IUser { };
+  // Archivos marcados para borrar - files to delete
+  const filesToDelete = filesToEvaluate.map(file => file.flagged);
 
-    // Mejor
-    class User { };
-    interface User { };
+  // Mal, información técnica innecesaria
+  class AbstractUser { };
+  class UserMixin { };
+  class UserImplementation { };
+  interface IUser { };
+
+  // Mejor
+  class User { };
+  interface User { };
 
 
-    // Todo: Tarea
-        
-    // día de hoy - today
-    const ddmmyyyy = new Date();
-    
-    // días transcurridos - elapsed time in days
-    const d: number = 23;
-    
-    // número de archivos en un directorio - number of files in directory
-    const dir = 33;
-    
-    // primer nombre - first name
-    const nombre = 'Fernando';
-    
-    // primer apellido - last name
-    const apellido = 'Herrera';
+  // Todo: Tarea
 
-    // días desde la última modificación - days since modification
-    const dsm = 12;
-    
-    // cantidad máxima de clases por estudiante - max classes per student
-    const max = 6;
+  // día de hoy - today
+  const todaysDate = new Date();
+
+  // días transcurridos - elapsed time in days
+  const elapsedTimeInDays: number = 23;
+
+  // número de archivos en un directorio - number of files in directory
+  const numberOfFilesinDirectory = 33;
+
+  // primer nombre - first name
+  const firstName = 'Daniel';
+
+  // primer apellido - last name
+  const lastName = 'Gómez';
+
+  // días desde la última modificación - days since modification
+  const daysSinceModification = 12;
+
+  // cantidad máxima de clases por estudiante - max classes per student
+  const maxClassesPerStudent = 6;
 
 
 })();
