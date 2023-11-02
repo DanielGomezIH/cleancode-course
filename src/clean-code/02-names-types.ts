e9e04a4e42dbcd13ad831f3cbb76961b60e7814b
Daniel Gómez <danielivgho5@gmail.com>
2023-11-02 16:56:08 -0500

    archivo 02-names-types.ts creado

---
+++ src/clean-code/02-names-types.ts
@@ -0,0 +1,53 @@
+(() => {
+
+  const celciusTemperature = [33.6, 12.34];
+
+  const ipServerAddress = '123.123.123.123';
+
+  const listOfUsers = [{ id: 1, email: 'fernando@google.com' }, { id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+
+  const listOfUserEmails = listOfUsers.map(user => user.email);
+
+  // Variables booleanas de un video juego
+  const canJump = false;
+  const canRun = true;
+  const hasItems = true;
+  const isLoading = false;
+
+  // Otros ejercicios
+  // tiempo inicial
+  const startTime = new Date().getTime();
+  //....
+  // 3 doritos después
+  //...
+  // Tiempo al final
+  const endTime = new Date().getTime() - startTime;
+
+
+  // Funciones
+  // Obtiene los libros
+  function getBooks() {
+    throw new Error('Function not implemented.');
+  }
+
+  // obtiene libros desde un URL
+  function getBooksFromUrl(url: string) {
+    throw new Error('Function not implemented.');
+  }
+
+  // obtiene el área de un cuadrado basado en sus lados
+  function getAreaOfaSquare(sides: number) {
+    throw new Error('Function not implemented.');
+  }
+
+  // imprime el trabajo
+  function printJob() {
+    throw new Error('Function not implemented.');
+  }
+
+
+})();
+
+
+
+
